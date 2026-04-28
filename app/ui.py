f51af336c9cb8e3207a9525b1aa1b9c5dd56d0be
Prince Appiah <princeappiah181@gmail.com>
2026-04-28 16:08:47 -0600

    update app

--- app/ui.py
+++ app/ui.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 import pandas as pd
 
-API_URL = "http://127.0.0.1:8000/predict"
+API_URL = "http://127.0.0.1:8001/predict"
 
 st.set_page_config(
     page_title="AI Support Risk Analyzer",
